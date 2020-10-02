161716067e22e87ea4ac62c0aa8207c82fbd3e79
Marten Muru <marten.muru@nortal.com>
2020-10-02 10:02:33 +0300

    inital

---
+++ src/test/java/com/example/eidasauthserver/EidasAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eidasauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EidasAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
